9efc05d6758623e8881f55309770e82a69ae3484
Michael Wojnar <michael.j.wojnar@gmail.com>
2018-09-01 11:00:40 -0500

    Second wave of image imports

--- src/AssetLoader.py
+++ src/AssetLoader.py
@@ -7,12 +7,18 @@ class AssetLoader(object):
         self.spaceguyJump = Sprite(world, pygame.image.load("spr_spaceguy_jump.png"), 1)
         self.spaceguyFloat = Sprite(world, pygame.image.load("spr_spaceguy_float.png"), 1)
         self.spaceguyCrouch = Sprite(world, pygame.image.load("spr_spaceguy_crouch.png"), 1)
+        self.spaceguyDie = Sprite(world, pygame.image.load("spr_spaceguy_die_8frames.png"), 8)
         
-        self.mediumSpikes = Sprite(world, pygame.image.load("spr_medium_spikes1.png"), 1)
-        self.bigSpikes = Sprite(world, pygame.image.load("spr_big_spikes1.png"), 1)
+        self.smallSpikes = Sprite(world, pygame.image.load("spr_small_spikes.png"), 1)
+        self.mediumSpikes = Sprite(world, pygame.image.load("spr_medium_spikes.png"), 1)
+        self.bigSpikes = Sprite(world, pygame.image.load("spr_big_spikes.png"), 1)
         
         self.pullOrb = Sprite(world, pygame.image.load("spr_attract_4frames.png"), 4)
+        self.pullTether = Sprite(world, pygame.image.load("spr_attract_tether_3frames.png"), 3)
         
         self.bgEarth = Sprite(world, pygame.image.load("bg_earth.png"), 1)
         
         self.satellitePlatform = Sprite(world, pygame.image.load("ts_satellite_platform.png"), 1)
+        
+        self.arrowSegment = Sprite(world, pygame.image.load("spr_arrow_segment.png"), 1)
+        self.arrowTip = Sprite(world, pygame.image.load("spr_arrow_tip.png"), 1)
