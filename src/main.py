d3773ec1f94c858b8f2b372a6efec7823cbfc980
Michael Wojnar <michael.j.wojnar@gmail.com>
2018-09-02 11:26:27 -0500

    Random rotation of spikes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from World import World
 import pygame
+import random
 
 pygame.mixer.pre_init(44100, -16, 20, 2048)
 pygame.mixer.init()
 pygame.init()
 pygame.display.set_caption("In Space No One Can Hear You Jump")
+random.seed()
 width = 960
 height = 540
 windowSize = (width, height)
